4a5642dfcba9590a8df8c5d94d1689f11e64223c
crisjoacko <joacko724@gmail.com>
2025-11-14 01:34:37 +0100

    ajustando atributos del form

---
+++ Asset/JS/from.js
@@ -0,0 +1,6 @@
+const name = document.getElementById('name');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const form = document.getElementById('form');
